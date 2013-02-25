2bb5efbbf31f533e46791496d85ba23ff0961365
Jonas Baumann <jone@jone.ch>
2013-02-25 08:42:40 +0100

    Title and description are now searchable by default.

--- ftw/topics/__init__.py
+++ ftw/topics/__init__.py
@@ -1,12 +1,4 @@
 from zope.i18nmessageid import MessageFactory
-from plone.app.dexterity.behaviors.metadata import IBasic
-from collective.dexteritytextindexer.utils import searchable
 
 
 _ = MessageFactory('ftw.topics')
-
-# Mark default dexterity title and description as searchable
-# This should be later implemented in collective.dexterityindexer,
-# see https://github.com/collective/collective.dexteritytextindexer/issues/2
-searchable(IBasic, 'title')
-searchable(IBasic, 'description')
