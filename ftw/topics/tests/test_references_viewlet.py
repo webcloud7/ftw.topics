d6cc5e76d9bf3a4b0b35f0f11a3092e0b19247fb
Yves Senn <yves.senn@gmail.com>
2013-05-08 10:11:26 +0200

    Refactor topic viewlet tests.

--- ftw/topics/tests/test_references_viewlet.py
+++ ftw/topics/tests/test_references_viewlet.py
@@ -1,18 +1,17 @@
+from Products.Five.browser import BrowserView
 from ftw.topics.interfaces import ITopicBrowserLayer
 from ftw.topics.testing import SIMPLELAYOUT_TOPICS_INTEGRATION_TESTING
-from plone.app.testing import login
-from plone.app.testing import logout
-from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
-from Products.Five.browser import BrowserView
+from plone.app.testing import login
+from plone.app.testing import setRoles
 from unittest2 import TestCase
 from zope.component import queryMultiAdapter
 from zope.interface import alsoProvides
 from zope.viewlet.interfaces import IViewletManager
 
 
-class TestReferencesViewlet(TestCase):
+class TestReferencesViewletOnPage(TestCase):
 
     layer = SIMPLELAYOUT_TOPICS_INTEGRATION_TESTING
 
@@ -22,27 +21,56 @@ class TestReferencesViewlet(TestCase):
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         login(self.portal, TEST_USER_NAME)
 
-        self.tree = self.portal.get(
-            self.portal.invokeFactory('ftw.topics.TopicTree', 'tree'))
-
-        self.topic1 = self.tree.get(
-            self.tree.invokeFactory('ftw.topics.Topic', 'topic1',
-                                    title="A topic"))
-        self.topic2 = self.tree.get(
-            self.tree.invokeFactory('ftw.topics.Topic', 'topic2',
-                                    title="B topic"))
-
         self.page = self.portal.get(
             self.portal.invokeFactory('ContentPage', 'page'))
 
-    def tearDown(self):
-        super(TestReferencesViewlet, self).tearDown()
-        login(self.portal, TEST_USER_NAME)
+    def test_registered(self):
+        # Only Registered for ITopicSupport
+        self.assertFalse(self.viewlet(self.portal))
+        self.assertTrue(self.viewlet(self.page))
 
-        self.portal.manage_delObjects(['tree', 'page'])
-        logout()
+    def test_not_available_without_references(self):
+        self.assertFalse(self.viewlet().available())
 
-    def _get_viewlet(self, context):
+    def test_available_with_references(self):
+        topic1, topic2 = self.create_topic_tree("A topic", "B topic")
+
+        self.reference_topics(topic1, topic2)
+        self.assertTrue(self.viewlet().available())
+
+        result = [
+            dict(title='A topic',
+                 description='',
+                 url=topic1.absolute_url()),
+            dict(title='B topic',
+                 description='',
+                 url=topic2.absolute_url()), ]
+
+        self.assertEquals(result, self.viewlet().get_references())
+
+    def test_references_without_view_permissions_are_not_visible(self):
+        about_peter, = self.create_topic_tree("About Peter")
+
+        self.reference_topics(about_peter)
+        self.assert_references(['About Peter'])
+
+        about_peter.manage_permission('View', roles=[], acquire=False)
+        self.assert_references([])
+        self.assertFalse(self.viewlet().available(),
+                         "Viewlet without visible references should not be available")
+
+    def create_topic_tree(self, *topic_titles):
+        tree = self.portal.get(
+            self.portal.invokeFactory('ftw.topics.TopicTree', 'tree'))
+
+        for i, topic_title in enumerate(topic_titles):
+            tree.invokeFactory('ftw.topics.Topic', 'topic%i' % i,
+                               title=topic_title)
+        return tree.objectValues()
+
+    def viewlet(self, context=None):
+        if context is None:
+            context = self.page
         alsoProvides(context.REQUEST, ITopicBrowserLayer)
 
         view = BrowserView(context, context.REQUEST)
@@ -56,41 +84,18 @@ class TestReferencesViewlet(TestCase):
         # Set up viewlets
         manager.update()
         name = 'ftw.topics.references'
-        return [v for v in manager.viewlets if v.__name__ == name]
+        viewlets = [v for v in manager.viewlets if v.__name__ == name]
+        if len(viewlets) == 0:
+            return None
+        else:
+            self.assertEquals(1, len(viewlets),
+                              "There should only be one viewlet!")
+            return viewlets[0]
 
-    def test_registered(self):
-        # Only Registered for ITopicSupport
-        self.assertFalse(self._get_viewlet(self.portal))
-
-        self.assertTrue(self._get_viewlet(self.page))
-
-    def test_references(self):
-        viewlet = self._get_viewlet(self.page)[0]
-        # No references
-        self.assertFalse(viewlet.available())
-
+    def reference_topics(self, *topics):
         self.page.Schema()['topics'].set(
-            self.page, (self.topic2.UID(), self.topic1.UID()))
+            self.page, [t.UID() for t in topics])
 
-        viewlet = self._get_viewlet(self.page)
-        result = [
-            dict(title='A topic',
-                 description='',
-                 url=self.topic1.absolute_url()),
-            dict(title='B topic',
-                 description='',
-                 url=self.topic2.absolute_url()), ]
-        self.assertEquals(viewlet[0].get_references(), result)
-
-    def test_permission_on_references(self):
-        self.page.Schema()['topics'].set(
-            self.page, (self.topic2.UID(), self.topic1.UID()))
-
-        self.topic2.manage_permission('View', roles=[], acquire=False)
-
-        viewlet = self._get_viewlet(self.page)
-        result = [
-            dict(title='A topic',
-                 description='',
-                 url=self.topic1.absolute_url()), ]
-        self.assertEquals(viewlet[0].get_references(), result)
+    def assert_references(self, references):
+        reference_titles = [v['title'] for v in self.viewlet().get_references()]
+        self.assertEquals(references, reference_titles)
