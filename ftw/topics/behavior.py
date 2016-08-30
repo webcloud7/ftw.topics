e1da0793b5b24375aa21fb5d37ba524886c5ad44
Timon Tschanz <timon.tschanz@gmail.com>
2016-08-30 11:31:03 +0200

    use ftw.referencewidget

--- ftw/topics/behavior.py
+++ ftw/topics/behavior.py
@@ -1,34 +1,20 @@
+from ftw.referencewidget.widget import ReferenceBrowserWidget
 from ftw.topics import _
 from ftw.topics.interfaces import ITopicRootFinder
 from plone.app.relationfield.event import extract_relations
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.directives.form import widget
-from plone.formwidget.contenttree import MultiContentTreeFieldWidget
-from plone.formwidget.contenttree import ObjPathSourceBinder
-from plone.formwidget.contenttree.utils import closest_content
 from z3c.relationfield.event import _setRelation
-from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
 from zope.component import getMultiAdapter
 from zope.interface import alsoProvides
 from zope.interface import Interface
 
 
-class TopicsObjPathSourceBinder(ObjPathSourceBinder):
-
-    def __call__(self, context):
-        if not self.navigation_tree_query:
-            self.navigation_tree_query = {}
-
-        finder = getMultiAdapter((context, context.REQUEST),
-                                 ITopicRootFinder)
-        self.navigation_tree_query['path'] = {
-            'query': finder.get_topic_root_path()}
-
-        return self.path_source(
-            closest_content(context),
-            selectable_filter=self.selectable_filter,
-            navigation_tree_query=self.navigation_tree_query)
+def get_topic_root(widget):
+    finder = getMultiAdapter((widget.context, widget.request),
+                             ITopicRootFinder)
+    return finder.get_topic_root_path()
 
 
 class ITopicSupportSchema(Interface):
@@ -36,13 +22,15 @@ class ITopicSupportSchema(Interface):
     a reference field `topics` to the content type.
     """
 
-    widget(topics=MultiContentTreeFieldWidget)
+    widget('topics',
+           ReferenceBrowserWidget,
+           override=True,
+           start=get_topic_root,
+           allow_traversal=["ftw.topics.Topic", "ftw.topics.TopicTree"],
+           selectable=["ftw.topics.Topic"])
     topics = RelationList(
         title=_(u'label_topics', default=u'Topics'),
-        value_type=RelationChoice(
-            source=TopicsObjPathSourceBinder(
-                # portal_type=['ftw.topics.Topic']
-                )))
+    )
 
 
 alsoProvides(ITopicSupportSchema, IFormFieldProvider)
